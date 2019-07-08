dd592c055f6c6187e6838c5cc1e1355a0ee364c4
Ha T. Le <51522947+hatmle@users.noreply.github.com>
2019-07-08 14:06:54 -0400

    Update basic-join.sql

--- basic-join.sql
+++ basic-join.sql
@@ -51,3 +51,19 @@ FROM COUNTRY
 INNER JOIN CITY
 ON CITY.CountryCode = COUNTRY.Code
 GROUP BY COUNTRY.CONTINENT;
+
+/* The Report
+Two tables: Students and Grades. Students contains three columns ID, Name and Marks.
+Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. Ketty doesn't want the NAMES of those 
+students who received a grade lower than 8. The report must be in descending order by grade -- i.e. higher grades are entered first.
+If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name 
+alphabetically. Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order. 
+If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in 
+ascending order.
+Write a query to help Eve. */
+
+select if(grades.grade<8,concat("NULL"),students.name),grades.grade,students.marks 
+from students 
+inner join grades 
+on(students.marks>=grades.min_mark and students.marks<=grades.max_mark ) 
+group by grades.grade desc, students.name,students.marks desc;
