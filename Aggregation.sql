219011d41b96b164d9c77eac1bc5537cb94c7066
Ha T. Le <51522947+hatmle@users.noreply.github.com>
2019-07-23 13:44:52 -0400

    Update Aggregation.sql

--- Aggregation.sql
+++ Aggregation.sql
@@ -1,6 +1,13 @@
 /* Average Population
 Query the average population for all cities in CITY, rounded down to the nearest integer. */
 
+CREATE TABLE CITY 
+(ID NUMBER, 
+NAME VARCHAR2(17),
+COUNTRYCODE VARCHAR2(3),
+DISTRICT VARCHAR2(20),
+POPULATION NUMBER);
+
 SELECT ROUND(AVG(POPULATION),0)
 FROM CITY;
 
@@ -13,3 +20,8 @@ WHERE COUNTRYCODE = 'JPN';
 -- Query the difference between the maximum and minimum populations in CITY.
 SELECT MAX(POPULATION)-MIN(POPULATION)
 FROM CITY;
+
+-- Query a count of the number of cities in CITY having a Population larger than 100,000.
+SELECT COUNT(*)
+FROM CITY
+WHERE POPULATION > 100000;
